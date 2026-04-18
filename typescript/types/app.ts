9c6a29fa1e82ef48c318e2cfbf9534218391774b
kalyanAIML <kalyan.erpln@gmail.com>
2026-04-18 16:39:09 +0530

    Create app.ts

---
+++ typescript/types/app.ts
@@ -0,0 +1,5 @@
+import { add, subtract, multiply } from "./calculator";
+
+console.log("Addition:", add(10, 5));
+console.log("Subtraction:", subtract(10, 5));
+console.log("Multiplication:", multiply(10, 5));
