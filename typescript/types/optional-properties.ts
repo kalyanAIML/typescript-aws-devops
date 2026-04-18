ad5035d1b5b2e46bc5f096d17f8fb473ddcfb462
kalyanAIML <kalyan.erpln@gmail.com>
2026-04-18 15:54:22 +0530

    Create optional-properties.ts

---
+++ typescript/types/optional-properties.ts
@@ -0,0 +1,16 @@
+type User = {
+  name: string;
+  age?: number;
+};
+
+const user1: User = {
+  name: "Kalyan"
+};
+
+const user2: User = {
+  name: "Ravi",
+  age: 30
+};
+
+console.log(user1);
+console.log(user2);
