dce9034fa72cb16a15301fdf4579d99eb0533bb6
kalyanAIML <kalyan.erpln@gmail.com>
2026-04-18 15:54:53 +0530

    Update return-type.ts

--- typescript/types/return-type.ts
+++ typescript/types/return-type.ts
@@ -3,3 +3,10 @@ let productNames: string[] = ["Laptop", "Mobile", "Keyboard"];
 
 console.log(prices);
 console.log(productNames);
+
+
+function sum(a: number, b: number): number {
+  return a + b;
+}
+
+console.log(sum(10, 20));
